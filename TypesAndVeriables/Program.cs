277a8ef0376167ea3eb2e108e5e8ba46539d3d50
BuildTools <unconfigured@null.spigotmc.org>
2024-11-23 14:00:19 +0300

    Add project files.

---
+++ TypesAndVeriables/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVeriables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello World!");
+            //int = +2147483647 veya -2147483648 ile sınırlandırılır.
+            double number5 = 10.4;
+            decimal number6 = 10;
+            char character = 'A';
+            bool condition = false;
+            byte number4 = 255;
+            short number3 = 32767;
+            int number1 = 2147483647;
+            long number2 = 9223372036854775807;
+            var number7 = 10;
+            number7 = 'A';
+            Console.WriteLine("Number 1 is {0}", number1);
+            Console.WriteLine("Number 2 is {0}", number2);
+            Console.WriteLine("Number 3 is {0}", number3);
+            Console.WriteLine("Number 4 is {0}", number4);
+            Console.WriteLine("Number 5 is {0}", number5);
+            Console.WriteLine("Number 7 is {0}", number7);
+            Console.WriteLine("Character is {0}",(int)character);
+            Console.WriteLine((int)Days.Friday);    
+            Console.ReadLine();
+        }
+    }
+    enum Days
+    {
+        Monday=10,Tuesday=20,Wednesday=30,Thursday,Friday,Saturday,Sunday
+    }
+}
